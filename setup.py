6d5fd1e8cf5429e5eed74861b53bee9a0a55c55f
Adel Ardalan <adel@cs.wisc.edu>
2020-05-03 16:17:35 -0400

    [NEW] First commit.

--- setup.py
+++ setup.py
@@ -68,11 +68,6 @@ if __name__ == "__main__":
             'Operating System :: MacOS',
             'Operating System :: Microsoft :: Windows',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',                            
             'Programming Language :: Python :: 3.7', 
             'Topic :: Scientific/Engineering',
             'Topic :: Utilities',
@@ -80,7 +75,6 @@ if __name__ == "__main__":
         ],
         packages=packages,
         install_requires=[
-            "python~=3.7.0",
             "setuptools",
             "pandas",
             "nose",
@@ -90,7 +84,6 @@ if __name__ == "__main__":
             "pywavelets"
         ],
         setup_requires=[
-            "python~=3.7.0",
             "setuptools",
             "pandas",
             "nose",
