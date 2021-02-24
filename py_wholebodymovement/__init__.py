86e19ec60a0667aad1733f68a0a4d4da729637a2
Adel Ardalan <adel@cs.wisc.edu>
2021-02-24 14:47:23 -0500

    [REFACTOR] Improved efficiency of AF angle calculations.

--- py_wholebodymovement/__init__.py
+++ py_wholebodymovement/__init__.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.Y.ZaN   # Alpha release
+#   X.Y.ZbN   # Beta release
+#   X.Y.ZrcN  # Release Candidate
+#   X.Y.Z     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.1.0'
+
 from py_wholebodymovement.articulated_figure import calculate_2d_articulated_figure_angle
 from py_wholebodymovement.articulated_figure import calculate_2d_articulated_figure_angles
 from py_wholebodymovement.articulated_figure import extend_2d_articulated_figure
